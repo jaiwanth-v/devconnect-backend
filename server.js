41f790265f92f2a0aba57a1ed11cc25b2d67569d
Jaiwanth <jaiwanth2011@gmail.com>
2020-08-16 19:30:54 +0530

    small changes

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token, PUT, GET, POST, DELETE"
+    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token"
   );
+
+  res.header("Access-Control-Allow-Methods", "GET,PUT, POST, DELETE");
+
   next();
 });
 
